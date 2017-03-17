a0a2b962f1d6fc63d05945e18c765196f457de69
caoye <caoye@gomeplus.com>
2017-03-17 11:06:28 +0800

    Initial Commit

---
+++ PreventCrash/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PreventCrash
+//
+//  Created by caoye on 17/3/17.
+//  Copyright © 2017年 caoye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PreventCrash/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PreventCrash
+//
+//  Created by caoye on 17/3/17.
+//  Copyright © 2017年 caoye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
